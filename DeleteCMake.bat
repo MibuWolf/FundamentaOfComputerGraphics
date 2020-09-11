621b937956f8dd67455ea49292311a64f87721b5
MibuWolf <lilin@imofly.com>
2020-09-11 21:56:21 +0800

    删除文件批处理

---
+++ DeleteCMake.bat
@@ -0,0 +1,13 @@
+del.vs /Q
+del CMakeFiles /Q
+del Debug /Q
+del FundamentaOfComputerGraphics.dir /Q
+del Win32 /Q
+del x64 /Q
+del ALL_BUILD.* /Q
+del cmake_install.cmake /Q
+del CMakeCache.txt /Q
+del *.vcxproj /Q
+del *.vcxproj.filters /Q
+del *.vcxproj.user /Q
+del *.sln /Q
